7550e9f856836a0a12b63593b16ce697d4732db9
morolost <moronacho57@gmail.com>
2024-08-20 17:09:13 -0300

    subo proyecto

---
+++ compu/New Text Document.py
@@ -0,0 +1,32 @@
+import os
+
+cadena = "marcos"
+
+entero = 12
+
+flotante = 12.5
+
+boolean = True
+
+a = 10
+
+b = 10
+
+b = 14
+
+###if a == 30:
+#    print("soy a valgo 20")
+
+#if b > a:
+#    print("soy mayor que a")
+#else:
+#    print("no paso nada papi")
+#if a != b :
+#    if b < a:
+#        print("soy diferente")
+#    else:
+#        print("no soy menor que a")
+for i in range (10):
+    print(i)  
+
+os.system("pause")
---
+++ tarea/20-08.py
@@ -0,0 +1,5 @@
+a = 0
+for i in range  (5):
+    a += 1
+if a != 4:
+    print("soy mayor a 4")
