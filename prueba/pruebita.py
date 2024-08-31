3f2bed5cf4eca3f2e2afe5a47ab4f1c76f6c3849
morolost <moronacho57@gmail.com>
2024-08-31 12:59:08 -0300

    archivo actualizado

---
+++ prueba/pruebita.py
@@ -0,0 +1,24 @@
+import os
+os.system("cls")
+
+a = int(-4)
+b = abs (a)
+c = pow (b, 2)
+d = str("object")
+e = 14.14
+f = sum([c, e])
+g = round(f)
+h = max(f, c)
+i = float(g)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
+os.system("pause")
